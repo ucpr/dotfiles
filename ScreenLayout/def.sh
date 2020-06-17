79428057387c843b35b587ba70693ed93d7d9392
ucpr <contact@ucpr.dev>
2020-06-17 22:21:04 +0900

    feat: initial commit

---
+++ ScreenLayout/def.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --primary --mode 2560x1440 --pos 0x1050 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --mode 1680x1050 --pos 440x0 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off
---
+++ ScreenLayout/home.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output eDP1 --primary --mode 2560x1440 --pos 440x1440 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --mode 3440x1440 --pos 0x0 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off
+/home/ucpr/.fehbg
