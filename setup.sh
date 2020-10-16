179f4a21b7097a02be44b41597ee62cf1838b961
ucpr <contact@ucpr.dev>
2020-10-16 23:44:45 +0900

    feat: update

--- setup.sh
+++ setup.sh
@@ -1,35 +1,71 @@
 #!/bin/sh
 
-mkdir ~/.vim
-mkdir ~/.config/nvim/
+cat << EOF
 
-cp ./init.vim ~/.config/nvim/init.vim
-cp -r ./toml ~/.vim/toml
-cp ./.zshrc ~/.zshrc
-cp ./.bash_aliases ~/.bash_aliases
-cp ./.curlrc ~/.curlrc
-cp ./.wgetrc ~/.wgetrc
-cp ./.latexmkrc ~/.latexmkrc
-cp ./.tmux.conf ~/.tmux.conf
-cp ./.Xmodmap ~/.Xmodmap
+     _       _    __ _ _
+  __| | ___ | |_ / _(_) | ___  ___
+ / _  |/ _ \| __| |_| | |/ _ \/ __|
+| (_| | (_) | |_|  _| | |  __/\__ \
+ \__,_|\___/ \__|_| |_|_|\___||___/
 
-# git
-git config --global user.email "uchihara1229@gmail.com"
-git config --global user.name "nve3pd"
+  https://github.com/ucpr/dotfiles
+
+
+EOF
+
+# ------------------------- init ----------------------------- #
+
+# Ubuntu
+echo "$(cat /etc/issue)" | grep "Ubuntu"
+if [ "$?" -eq 1 ]
+then
+  sudo apt update -y -q
+  sudo apt upgrade -y -q
+  sudo apt install -y -q build-essential curl vim zsh git
+fi
+
+# Archlinux
+echo "$(cat /etc/issue)" | grep "Arch Linux"
+if [ "$?" -eq 1 ]
+then
+  sudo pacman -Syu --noconfirm
+  sudo pacman -S --noconfirm git vim zsh curl
+fi
+
+# ------------------------------------------------------------ #
+
+# Create symbolic link
+echo "Create symbolic link"
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/.Xmodmap ~/.Xmodmap
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/vim ~/.vim
+ln -s ~/.dotfiles/nvim ~/.config/nvim
+ln -s ~/.dotfiles/ranger ~/.config/ranger
+ln -s ~/.dotfiles/ScreenLayout ~/.config/ScreenLayout
+ln -s ~/.dotfiles/i3 ~/.config/i3
+ln -s ~/.dotfiles/alacritty ~/.config/alacritty
+ln -s ~/.dotfiles/polybar ~/.config/polybar
+
+# Setting git
+echo "Setting git"
+git config --global user.email "contact@ucpr.dev"
+git config --global user.name "ucpr"
 
 # dein.vim
-echo "install dein.vim"
+echo "Clone dein.vim"
 mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim
 git clone https://github.com/Shougo/dein.vim.git ~/.vim/dein/repos/github.com/Shougo/dein.vim
 
-# Ubuntu
-echo "$(uname -r)" | grep Ubuntu
-if [ "$?" -eq 1 ]
-then
-  sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
-		libsqlite3-dev wget curl llvm libfreetype6-dev libblas-dev liblapack-dev gfortran tk-dev python3-tk
-  sudo add-apt-repository ppa:neovim-ppa/unstable
-  sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
-  sudo apt update -y
-  sudo apt install -y neovim ubuntu-make zsh
-fi
+# Tmux Plugin Manager
+echo "Clone Tmux Plugin Manager"
+mkdir -p ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# anyenv
+echo "Clone anyenv"
+git clone https://github.com/anyenv/anyenv ~/.anyenv
+export PATH="$HOME/.anyenv/bin:$PATH"
+anyenv init
+anyenv install --init
