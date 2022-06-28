4192cbc497da4a3de6853f708598244951ca4702
ucpr <contact@ucpr.dev>
2022-06-28 14:07:14 +0900

    feat: add alias

--- common/zsh/plugin.zsh
+++ common/zsh/plugin.zsh
@@ -43,6 +43,8 @@ alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 
+alias python='python3'
+
 ### Export
 export GOPATH=~/.go
 export PATH=~/.go/bin:$PATH
