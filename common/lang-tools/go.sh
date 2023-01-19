540b46280f0fa4b7eb6915537571f1efe7f8951a
ucpr <contact@ucpr.dev>
2023-01-19 17:22:48 +0900

    update dotfiles

--- common/lang-tools/go.sh
+++ common/lang-tools/go.sh
@@ -1,4 +1,4 @@
-go install golang.org/x/tools/gopls@latest
+# go install golang.org/x/tools/gopls@latest
 go install golang.org/x/tools/cmd/goimports@latest
 go install github.com/fatih/gomodifytags@latest
 go install github.com/rhysd/vim-startuptime@latest
