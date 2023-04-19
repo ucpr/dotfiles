bddb7567ffa7713cec181890e03f8793fad885ec
ucpr <contact@ucpr.dev>
2023-04-19 19:09:06 +0900

    feat: change colorscheme

--- common/nvim/init.lua
+++ common/nvim/init.lua
@@ -20,8 +20,8 @@ local options = {
   -- theme
   title = false,
   number = true,
-  cursorline = true,
-  cursorcolumn = true,
+  cursorline = false,
+  cursorcolumn = false,
   showmatch = true,
   wildmenu = true,
   list = true,
@@ -175,11 +175,35 @@ require('packer').startup(function(use)
 
   -- etc
   use {
-    "sainnhe/everforest",
+    "navarasu/onedark.nvim",
     config = function()
-      vim.cmd [[
-        colorscheme everforest
-      ]]
+      require("onedark").setup {
+        style = "warmer",
+        transparent = true,           -- Show/hide background
+        term_colors = true,           -- Change terminal color as per the selected theme style
+        ending_tildes = false,        -- Show the end-of-buffer tildes. By default they are hidden
+        cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+
+        code_style = {
+          comments = 'italic',
+          keywords = 'none',
+          functions = 'none',
+          strings = 'none',
+          variables = 'none'
+        },
+
+        -- Custom Highlights --
+        colors = {},     -- Override default colors
+        highlights = {}, -- Override highlight groups
+
+        -- Plugins Config --
+        diagnostics = {
+          darker = true,     -- darker colors for diagnostic
+          undercurl = true,  -- use undercurl instead of underline for diagnostics
+          background = true, -- use background color for virtual text
+        },
+      }
+      require("onedark").load()
     end
   }
   use { "ntpeters/vim-better-whitespace", opt = true }
--- common/wezterm/wezterm.lua
+++ common/wezterm/wezterm.lua
@@ -2,7 +2,6 @@ local wezterm = require "wezterm";
 
 local keys = {
   { key = "n", mods = "ALT", action = "ShowLauncher" },
-
   { key = "s", mods = "ALT", action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" } },
   { key = "v", mods = "ALT", action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" } },
   { key = "h", mods = "ALT", action = wezterm.action.ActivatePaneDirection "Left" },
@@ -20,32 +19,17 @@ for i = 1, 9 do
 end
 
 return {
-  colors = {
-    foreground = "#d3c6aa",
-    background = "#2b3339",
-    cursor_bg = "#d3c6aa",
-    cursor_border = "#d3c6aa",
-    cursor_fg = "#2b3339",
-    selection_bg = "#d3c6aa",
-    selection_fg = "#2b3339",
-    ansi = { "#4b565c", "#e67e80", "#a7c080", "#dbbc7f", "#7fbbb3", "#d699b6", "#83c092", "#d3c6aa" },
-    brights = { "#4b565c", "#e67e80", "#a7c080", "#dbbc7f", "#7fbbb3", "#d699b6", "#83c092", "#d3c6aa" },
-  },
-
+  color_scheme = 'One Dark (Gogh)',
   use_ime = true,
-
   font = wezterm.font "Hack Nerd Font",
   font_size = 15.0,
-
   window_padding = {
     left = 0,
     right = 0,
     top = 0,
     bottom = 0,
   },
-
   keys = keys,
-
-  window_background_opacity = 0.8,
+  window_background_opacity = 0.9,
   -- front_end = "WebGpu",
 }
