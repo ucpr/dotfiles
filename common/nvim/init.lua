01885ead1ebe070d498d61a2e48bd9acf84ea86e
ucpr <contact@ucpr.dev>
2023-04-24 23:15:47 +0900

    feat: add git diff to commit message

--- common/nvim/init.lua
+++ common/nvim/init.lua
@@ -523,6 +523,23 @@ require('packer').startup(function(use)
           -- keymap("i", "<C-k>", "<Plug>(copilot-previous)")
           -- keymap("i", "<C-o>", "<Plug>(copilot-dismiss)")
           -- keymap("i", "<C-s>", "<Plug>(copilot-suggest)")
+
+          local function append_diff()
+            -- Get the Git repository root directory
+            local git_dir = vim.fn.FugitiveGitDir()
+            local git_root = vim.fn.fnamemodify(git_dir, ':h')
+            -- Get the diff of the staged changes relative to the Git repository root
+            local diff = vim.fn.system('git -C ' .. git_root .. ' diff --cached')
+            -- Add a comment character to each line of the diff
+            local comment_diff = table.concat(vim.tbl_map(function(line)
+              return '# ' .. line
+            end, vim.split(diff, '\n')), "\n")
+            -- Append the diff to the commit message
+            vim.api.nvim_buf_set_lines(0, -1, -1, false, vim.split(comment_diff, '\n'))
+          end
+          vim.cmd [[
+            autocmd BufReadPost COMMIT_EDITMSG call lua append_diff()
+          ]]
         end,
       },
       -- {
@@ -712,6 +729,12 @@ require('packer').startup(function(use)
         autocmd User PumCompleteDone call vsnip_integ#on_complete_done(g:pum#completed_item)
       ]]
     end
+  }
+
+  -- figutive
+  use {
+    "tpope/vim-fugitive",
+    event = "InsertEnter",
   }
 
   -- go
