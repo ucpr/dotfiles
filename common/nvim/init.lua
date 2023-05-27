7f3e830e2296c5272ed010ae7e1b884707eb95db
ucpr <contact@ucpr.dev>
2023-05-28 02:52:15 +0900

    feat: add nvim-dap-go config

--- common/nvim/init.lua
+++ common/nvim/init.lua
@@ -818,7 +818,37 @@ require('packer').startup(function(use)
           })
         end,
       },
-      "leoluz/nvim-dap-go",
+      {
+        "leoluz/nvim-dap-go",
+        config = function()
+          require("dap-go").setup {
+            dap_configurations = {
+              {
+                -- Must be "go" or it will be ignored by the plugin
+                type = "go",
+                name = "Attach remote",
+                mode = "remote",
+                request = "attach",
+              },
+            },
+            -- delve configurations
+            delve = {
+              -- the path to the executable dlv which will be used for debugging.
+              -- by default, this is the "dlv" executable on your PATH.
+              path = "dlv",
+              -- time to wait for delve to initialize the debug session.
+              -- default to 20 seconds
+              initialize_timeout_sec = 20,
+              -- a string that defines the port to start delve debugger.
+              -- default to string "${port}" which instructs nvim-dap
+              -- to start the process in a random available port
+              port = "${port}",
+              -- additional args to pass to dlv
+              args = {}
+            },
+          }
+        end,
+      },
     },
   }
 
