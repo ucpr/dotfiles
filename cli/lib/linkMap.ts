45c7b4000ea9a6c4219b1b022a6670537b8c6faa
ucpr <contact@ucpr.dev>
2023-05-28 17:10:24 +0900

    fix: fix dotfiles dir

--- cli/lib/linkMap.ts
+++ cli/lib/linkMap.ts
@@ -11,40 +11,55 @@ interface config {
 export type LinkMap = Map<string, config>;
 
 const home = Deno.env.get("HOME") || "~/";
-const currentDir = Deno.cwd();
+const dotfilesDir = await (async (): Promise<string> => {
+  const command = new Deno.Command(
+    "git",
+    {
+      args: ["rev-parse", "--show-toplevel"],
+    },
+  );
+  const { code, stdout, stderr } = await command.output();
+  if (code === 0) {
+    return new TextDecoder().decode(stdout).trim();
+  } else {
+    console.error(new TextDecoder().decode(stderr));
+    return "";
+  }
+})();
+
 const linkMap: LinkMap = new Map([
   ["neovim", {
-    target: join(currentDir, "nvim"),
+    target: join(dotfilesDir, "nvim"),
     symlink: join(home, ".config", "nvim"),
     platform: "common",
   }],
   ["wezterm", {
-    target: join(currentDir, "wezterm"),
+    target: join(dotfilesDir, "wezterm"),
     symlink: join(home, ".config", "wezterm"),
     platform: "common",
   }],
   [".zshrc", {
-    target: join(currentDir, ".zshrc"),
+    target: join(dotfilesDir, ".zshrc"),
     symlink: join(home, ".zshrc"),
     platform: "common",
   }],
   ["zsh", {
-    target: join(currentDir, "zsh"),
+    target: join(dotfilesDir, "zsh"),
     symlink: join(home, ".config", "zsh"),
     platform: "common",
   }],
   ["k9s", {
-    target: join(currentDir, "k9s"),
+    target: join(dotfilesDir, "k9s"),
     symlink: join(home, ".k9s"),
     platform: "common",
   }],
   ["gitconfig", {
-    target: join(currentDir, "git/.gitconfig"),
+    target: join(dotfilesDir, "git/.gitconfig"),
     symlink: join(home, ".gitconfig"),
     platform: "common",
   }],
   ["gitmessage", {
-    target: join(currentDir, "git/.gitmessage"),
+    target: join(dotfilesDir, "git/.gitmessage"),
     symlink: join(home, ".gitmessage"),
     platform: "common",
   }],
