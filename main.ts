8daeb9ffa3041da96834dadb28a32d62b0922952
ucpr <contact@ucpr.dev>
2023-05-28 15:38:27 +0900

    chore: fix

--- main.ts
+++ main.ts
@@ -9,39 +9,40 @@ interface config {
 }
 
 const home = Deno.env.get("HOME") || "~/";
+const currentDir = Deno.cwd();
 const linkMap: Map<string, config> = new Map([
   ["neovim", {
-    target: join(Deno.cwd(), "nvim"),
+    target: join(currentDir, "nvim"),
     symlink: join(home, ".config", "nvim"),
     platform: "common",
   }],
   ["wezterm", {
-    target: join(Deno.cwd(), "wezterm"),
+    target: join(currentDir, "wezterm"),
     symlink: join(home, ".config", "wezterm"),
     platform: "common",
   }],
   [".zshrc", {
-    target: join(Deno.cwd(), ".zshrc"),
+    target: join(currentDir, ".zshrc"),
     symlink: join(home, ".zshrc"),
     platform: "common",
   }],
   ["zsh", {
-    target: join(Deno.cwd(), "zsh"),
+    target: join(currentDir, "zsh"),
     symlink: join(home, ".config", "zsh"),
     platform: "common",
   }],
   ["k9s", {
-    target: join(Deno.cwd(), "k9s"),
+    target: join(currentDir, "k9s"),
     symlink: join(home, ".k9s"),
     platform: "common",
   }],
   ["gitconfig", {
-    target: join(Deno.cwd(), "git/.gitconfig"),
+    target: join(currentDir, "git/.gitconfig"),
     symlink: join(home, ".gitconfig"),
     platform: "common",
   }],
   ["gitmessage", {
-    target: join(Deno.cwd(), "git/.gitmessage"),
+    target: join(currentDir, "git/.gitmessage"),
     symlink: join(home, ".gitmessage"),
     platform: "common",
   }],
