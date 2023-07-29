b0a186845b3800bc5a2082d67626f835939ea556
ucpr <contact@ucpr.dev>
2023-07-30 02:22:15 +0900

    chore: disable filesize condition

--- nvim/init.lua
+++ nvim/init.lua
@@ -612,14 +612,14 @@ require('packer').startup(function(use)
         auto_install = true,
         highlight = {
           enable = true,
-          -- disable = { "c", "rust" },
-          disable = function(_, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-              return true
-            end
-          end,
+          -- filesize で現状そんなに問題がないので一旦コメントアウトして様子を見る。問題なかったら完全に消す。
+          -- disable = function(_, buf)
+          --   local max_filesize = 100 * 1024 -- 100 KB
+          --   local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          --   if ok and stats and stats.size > max_filesize then
+          --     return true
+          --   end
+          -- end,
           additional_vim_regex_highlighting = false,
         },
         indent = {
