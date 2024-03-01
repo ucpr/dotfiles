fbccb4f876025d8523d646e7df00d00da1dbeeb6
ucpr <contact@ucpr.dev>
2024-03-02 00:06:09 +0900

    chore: add path

--- zsh/plugin.zsh
+++ zsh/plugin.zsh
@@ -38,6 +38,7 @@ source "$HOME/.config/op/plugins.sh"
 export GOPATH=~/.go
 export PATH=~/.go/bin:$PATH
 
+export PATH="/opt/homebrew/bin:$PATH"
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 
