fb0fd3017e9eec2ad0721d7990b8f133248e0a14
ucpr <contact@ucpr.dev>
2024-03-09 15:28:43 +0900

    feat: add karabiner config

--- cli/lib/linkMap.ts
+++ cli/lib/linkMap.ts
@@ -67,6 +67,11 @@ const linkMap: LinkMap = new Map([
     symlink: join(home, ".gitmessage"),
     platform: "common",
   }],
+  ["karabiner", {
+    target: join(dotfilesDir, "karabiner"),
+    symlink: join(home, ".config", "karabiner"),
+    platform: "darwin",
+  }],
 ]);
 
 export { dotfilesDir, home, linkMap };
