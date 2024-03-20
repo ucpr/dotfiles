51eb3ba0dd7b87f6e9d0858afe0d4e5ca582cb70
ucpr <contact@ucpr.dev>
2024-03-20 23:36:18 +0900

    chore: remove dap

--- nvim/init.lua
+++ nvim/init.lua
@@ -630,6 +630,7 @@ require('packer').startup(function(use)
       },
     },
   }
+
   -- treesitter
   use {
     "nvim-treesitter/nvim-treesitter",
@@ -803,102 +804,6 @@ require('packer').startup(function(use)
         end
       }
     end
-  }
-
-  -- nvim-dap
-  use {
-    "mfussenegger/nvim-dap",
-    requires = {
-      {
-        "rcarriga/nvim-dap-ui",
-        event = { 'VimEnter' },
-        config = function()
-          require("dapui").setup({
-            icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
-            mappings = {
-              -- Use a table to apply multiple mappings
-              expand = { "<CR>", "<2-LeftMouse>" },
-              open = "o",
-              remove = "d",
-              edit = "e",
-              repl = "r",
-              toggle = "t",
-            },
-            layouts = {
-              {
-                elements = {
-                  -- Elements can be strings or table with id and size keys.
-                  { id = "scopes", size = 0.25 },
-                  "breakpoints",
-                  "stacks",
-                  "watches",
-                },
-                size = 40, -- 40 columns
-                position = "left",
-              },
-              {
-                elements = {
-                  "repl",
-                },
-                size = 0.25, -- 25% of total lines
-                position = "bottom",
-              },
-            },
-            controls = {
-              -- Requires Neovim nightly (or 0.8 when released)
-              enabled = true,
-              -- Display controls in this element
-              element = "repl",
-              icons = {
-                pause = "",
-                play = "",
-                step_into = "",
-                step_over = "",
-                step_out = "",
-                step_back = "",
-                run_last = "↻",
-                terminate = "□",
-              },
-            },
-            floating = {
-              max_height = nil,  -- These can be integers or a float between 0 and 1.
-              max_width = nil,   -- Floats will be treated as percentage of your screen.
-              border = "single", -- Border style. Can be "single", "double" or "rounded"
-              mappings = {
-                close = { "q", "<Esc>" },
-              },
-            },
-            windows = { indent = 1 },
-            render = {
-              max_type_length = nil, -- Can be integer or nil.
-              max_value_lines = 100, -- Can be integer or nil.
-            }
-          })
-        end,
-      },
-      {
-        "leoluz/nvim-dap-go",
-        event = { 'VimEnter' },
-        config = function()
-          require("dap-go").setup {
-            dap_configurations = {
-              {
-                type = "go",
-                name = "Attach remote",
-                mode = "remote",
-                request = "attach",
-              },
-            },
-            delve = {
-              path = "dlv",
-              initialize_timeout_sec = 20,
-              port = "${port}",
-              args = {}
-            },
-          }
-        end,
-      },
-    },
   }
 
   -- go
