9ea03bbcf856881f62d8babce1dc37860084b4c0
ucpr <contact@ucpr.dev>
2024-10-27 01:15:35 +0900

    chore: add spell file

--- nvim/lua/plugins/project.lua
+++ nvim/lua/plugins/project.lua
@@ -15,7 +15,7 @@ return {
 
       -- All the patterns used to detect root dir, when **"pattern"** is in
       -- detection_methods
-      patterns = { ".git", "Makefile", "package.json", "go.mod", "init.lua", "Makefile" },
+      patterns = { "go.mod", "init.lua", "Makefile", ".git", "Makefile", "package.json" },
 
       -- Table of lsp clients to ignore by name
       -- eg: { "efm", ... }
