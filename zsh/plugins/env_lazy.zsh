437b3776f4ddc8529703e8786754c94d7d74337a
ucpr <contact@ucpr.dev>
2025-04-22 19:47:50 +0900

    chore: update

--- zsh/plugins/env_lazy.zsh
+++ zsh/plugins/env_lazy.zsh
@@ -4,5 +4,7 @@ export PATH=~/.go/bin:$PATH
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 
+export PATH="/Users/s11591/.codeium/windsurf/bin:$PATH"
+
 export VISUAL=nvim
 export EDITOR=nvim
