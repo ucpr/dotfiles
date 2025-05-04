5bfcc99195d3474829fba167a8769fb4e5caed06
ucpr <contact@ucpr.dev>
2025-05-05 00:23:05 +0900

    fix: fix lsp config

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -8,43 +8,16 @@ return {
   config = function()
     local nvim_lsp = require("lspconfig")
     local mason_lspconfig = require("mason-lspconfig")
-
-    local protocol = require("vim.lsp.protocol")
+    -- local protocol = require("vim.lsp.protocol")
 
     local on_attach = function(client, bufnr)
-      -- keybindings
-      local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-
-      local opts = { noremap = true, silent = true }
-      -- buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-      buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-      buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-      buf_set_keymap("n", "gvd", "<cmd>:vsp<CR><cmd>lua vim.lsp.buf.definition()<CR>", opts)
-      buf_set_keymap("n", "gsd", "<cmd>:sp<CR><cmd>lua vim.lsp.buf.definition()<CR>", opts)
-      buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-      buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-      buf_set_keymap('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-      buf_set_keymap("n", "gn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-      buf_set_keymap("n", "gf", "<cmd>lua vim.lsp.buf.format { async = true }<CR>", opts)
-      buf_set_keymap('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-      buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-      buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-      buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-      buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-      buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-      -- use actions-preview
-      -- buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-      buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-      buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-      buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-      buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-      -- format on save
       if client.server_capabilities.documentFormattingProvider then
         vim.api.nvim_create_autocmd("BufWritePre", {
           group = vim.api.nvim_create_augroup("Format", { clear = true }),
           buffer = bufnr,
           callback = function()
-            vim.lsp.buf.format()
+            vim.lsp.buf.format({ async = true })
+            -- vim.lsp.buf.format()
           end,
         })
       end
@@ -57,6 +30,7 @@ return {
     end
 
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
     mason_lspconfig.setup_handlers({
       function(server)
         nvim_lsp[server].setup({
@@ -64,23 +38,22 @@ return {
           capabilities = capabilities,
         })
       end,
+
       ["gopls"] = function()
         nvim_lsp["gopls"].setup({
           on_attach = on_attach,
           capabilities = capabilities,
-          opts = {
-            settings = {
-              gopls = {
-                env = {
-                  GOFLAGS = "-tags=integration,wireinject",
-                },
-                gofumpt = true,
-                analyses = {
-                  unusedparams = true,
-                  shadow = true,
-                },
-                staticcheck = true,
+          settings = {
+            gopls = {
+              env = {
+                GOFLAGS = "-tags=integration,wireinject",
               },
+              gofumpt = true,
+              analyses = {
+                unusedparams = true,
+                shadow = true,
+              },
+              staticcheck = true,
             },
           },
         })
