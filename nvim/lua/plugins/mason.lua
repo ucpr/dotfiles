78d66ede4e38e5d67064e48ae52a37585f589142
ucpr <contact@ucpr.dev>
2025-05-11 18:25:07 +0900

    chore: pin mason

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,8 +1,15 @@
 return {
-  "williamboman/mason.nvim",
+  "mason-org/mason.nvim",
+  version = "v1.32.0",
   dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    {
+      "williamboman/mason-lspconfig.nvim",
+      version = "v1.32.0",
+    },
+    {
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      commit = "4aa03a08c3705e622f2e7886783fd450f7749cdd",
+    },
   },
   config = function()
     require("mason").setup()
@@ -22,8 +29,8 @@ return {
 
     require("mason-tool-installer").setup({
       ensure_installed = {
-        "stylua",         -- lua formatter
-        "black",          -- python formatter
+        "stylua", -- lua formatter
+        "black",  -- python formatter
         'lua-language-server',
         'vim-language-server',
         'gopls',
